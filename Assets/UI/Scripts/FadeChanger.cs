10e58fbaf04a1a58dc15c1775f2a35eddbebd3bb
glandogear <talk5600@gmail.com>
2019-09-22 11:11:05 +0800

    Add simple UI.

---
+++ Assets/UI/Scripts/FadeChanger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeChanger : MonoBehaviour
+{
+
+    public Animator animator;
+    private string sceneToLoad;
+
+    private enum Operations {
+        Scene,
+        Exit
+    }
+
+    private Operations ops;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeToScene(string newScene) {
+        ops = Operations.Scene;
+        sceneToLoad = newScene;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void FadeAndExit() {
+        ops = Operations.Exit;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete() {
+        switch(ops) {
+            case Operations.Scene:
+                SceneManager.LoadScene(sceneToLoad);
+                break;
+            case Operations.Exit:
+                Application.Quit();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // public void FadeAndExit() {
+    //     animator
+    // }
+}
